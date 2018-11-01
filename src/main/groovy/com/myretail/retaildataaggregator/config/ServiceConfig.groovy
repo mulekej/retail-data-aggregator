f95a8e73ebf3d8b8443f47b8109d5c506be16dd6
Eric Mulek <eric.mulek@surescripts.com>
2018-11-01 17:10:35 -0500

    add rest template

--- src/main/groovy/com/myretail/retaildataaggregator/config/ServiceConfig.groovy
+++ src/main/groovy/com/myretail/retaildataaggregator/config/ServiceConfig.groovy
@@ -1,5 +1,15 @@
 package com.myretail.retaildataaggregator.config
 
 
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.client.RestTemplate
+
+@Configuration
 class ServiceConfig {
+
+    @Bean
+    RestTemplate restTemplate(){
+        new RestTemplate()
+    }
 }
