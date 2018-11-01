24729ede784877bf334bb65982f05e21e4f3a668
Eric Mulek <eric.mulek@surescripts.com>
2018-11-01 15:45:51 -0500

    initial commit

---
+++ src/main/groovy/com/myretail/retaildataaggregator/RetailDataAggregatorApplication.groovy
@@ -0,0 +1,12 @@
+package com.myretail.retaildataaggregator
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class RetailDataAggregatorApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run RetailDataAggregatorApplication, args
+    }
+}
