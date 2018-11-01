9930db1586e00803af2cbfc6504897f7c54033fb
Eric Mulek <eric.mulek@surescripts.com>
2018-11-01 17:01:50 -0500

    plumb out controller

--- src/main/groovy/com/myretail/retaildataaggregator/controller/ProductsController.groovy
+++ src/main/groovy/com/myretail/retaildataaggregator/controller/ProductsController.groovy
@@ -1,4 +1,26 @@
 package com.myretail.retaildataaggregator.controller
 
+import com.myretail.retaildataaggregator.domain.api.Product
+import com.myretail.retaildataaggregator.services.AggregatorService
+import groovy.util.logging.Slf4j
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+import javax.annotation.Resource
+import javax.websocket.server.PathParam
+
+
+@RequestMapping(value = ["/api/v1/products"])
+@RestController
+@Slf4j
 class ProductsController {
+
+    @Resource
+    AggregatorService aggregatorService
+
+    @GetMapping(value = "{id}")
+    Product getProductInfoById(@PathParam("id") String productId) {
+        aggregatorService.getProductInfoById(productId)
+    }
 }
--- src/main/groovy/com/myretail/retaildataaggregator/services/AggregatorService.groovy
+++ src/main/groovy/com/myretail/retaildataaggregator/services/AggregatorService.groovy
@@ -1,5 +1,21 @@
 package com.myretail.retaildataaggregator.services
 
+import com.myretail.retaildataaggregator.domain.api.Product
+import com.myretail.retaildataaggregator.services.redsky.RedskyService
+import org.springframework.stereotype.Service
 
+import javax.annotation.Resource
+
+@Service
 class AggregatorService {
+
+    @Resource
+    RedskyService redskyService
+
+    Product getProductInfoById(String productId) {
+
+        new Product(id: productId)
+    }
+
+
 }
